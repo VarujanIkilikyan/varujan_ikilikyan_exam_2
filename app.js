25ec3f24956f9489e5bf63bd0f9dee1e29dfce68
varujan <varujanna@gmail.com>
2026-05-01 20:10:47 +0400

    start create enter point

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import 'dotenv/config.js'
 import {createServer} from 'http'
+import path from 'path'
+import morgan from 'morgan'
+
+import routes from './routes/index.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 const {PORT} = process.env || 3000;
